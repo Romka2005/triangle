eb547428073d7705981efd48706d2ea720370338
Romka2005 <svetanok12@gmail.com>
2022-01-17 00:54:37 +0200

    резюме

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ let b = document.getElementById('b');
 let c = document.getElementById('c');
 let triangle = document.querySelector('.triangle-bottom-left');
 let ds = document.getElementById('ds');
+let bg = document.getElementById('bg');
+
 function trianglesbm() {
     console.log(a.value);
     console.log(triangle);
-    
-    triangle.style.borderBottom = a.value * 37 + "px solid #5bc0de";
+    console.log(bg.value);
+
+    triangle.style.borderBottom = a.value * 37 + "px solid " + bg.value;
     triangle.style.borderRight= b.value * 37 + "px solid transparent";
     let result = Math.floor(Math.sqrt(Math.pow(a.value, 2) + Math.pow(b.value, 2)));
     ds.innerHTML = "Гипотенуза равна: " + result;
@@ -18,6 +21,7 @@ let a2 = document.getElementById('a2');
 let b2 = document.getElementById('b2');
 let c2 = document.getElementById('c2');
 let triangle2 = document.querySelector('.triangle-top2');
+let bg2 = document.getElementById('bg2');
 
 function trianglesbm2() {
     console.log(a2.value);
@@ -25,4 +29,5 @@ function trianglesbm2() {
     
     triangle2.style.borderLeft = a2.value * 37 + "px solid transparent";
     triangle2.style.borderRight = b2.value * 37 + "px solid transparent";
+    triangle2.style.borderBottom = a2.value * 37 + "px solid " + bg2.value;
 }
